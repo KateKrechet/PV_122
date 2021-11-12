cc441c0ceb17ac6fd52a51d2114a9af5b0a16bd0
KateKrechet <k.krechet@yandex.ru>
2021-11-12 18:42:30 +0300

    CW11.11

--- IntroductionToOOP/String/Source.cpp
+++ IntroductionToOOP/String/Source.cpp
@@ -1,13 +1,25 @@
 #include<iostream> 
 #include<string>
 using namespace std;
+
+class String;
+String operator+(const String& left, const String& right);
+
 class String
 {
 	int size;//размер строки в байтах
 	char* str;//адркс строки в динамической памяти
 
 public:
-	const char* get_str() const
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
 	{
 		return str;
 	}
@@ -32,6 +44,14 @@ public:
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
+	String(String&& other)
+	{
+		this->size = other.size;
+		this->str = other.str;
+		cout << "MoveConstructor:\t" << this << endl;
+		other.str = nullptr;
+		other.size = 0;
+	}
 	~String()
 	{
 		delete[]this->str;
@@ -51,11 +71,38 @@ public:
 		return *this;
 	}
 
+	String& operator=(String&& other)
+	{
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		other.size;
+		return *this;
+
+	}
+
+	String operator+=(const String& right)
+	{
+		return  *this = *this + right;
+	}
+
+	const char& operator[](int i)const
+	{
+		return str[i];
+
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+
+	}
+
 	void print()const
 	{
 		cout << "Size:\t" << size << endl;
 		cout << "Str:\t" << str << endl;
 	}
+	
 
 };
 
@@ -64,14 +111,27 @@ ostream& operator<<(ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
-String operator+(String left, String right)
+String operator+(const String& left, const String& right)
 {
-	
+	String buffer = left.get_size() + right.get_size() - 1;
+	/*for (int i = 0; i < left.get_size(); i++)
+		buffer.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i];*/
+	for (int i = 0; i < left.get_size(); i++)
+		buffer[i] = left[i];
+	for (int i = 0; i < right.get_size(); i++)
+		buffer[i + left.get_size() - 1] = right[i];
+
+	return buffer;
 }
+
+
 	
 
 
 //#define CONSTRUCTORS_CHECK
+#define delimiter "\n---------------------------------------\n"
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -94,7 +154,13 @@ void main()
 	cout << str1 << endl;
 	String str2 = "World";
 	cout << str2 << endl;
+	cout << delimiter << endl;
 	String str3 = str1 + str2;
+	cout << delimiter << endl;
 	cout << str3 << endl;
+	cout << delimiter << endl;
+	str1 += str2;
+	cout << delimiter << endl;
+	cout << str1 << endl;
 
 }

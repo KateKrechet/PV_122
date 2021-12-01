dcf2f6b372d7d182ad3e82bf18df7e4a78afb29f
KateKrechet <k.krechet@yandex.ru>
2021-12-02 00:37:12 +0300

    Home work in progress

--- Inheritance/AbstractGeometry/Source.cpp
+++ Inheritance/AbstractGeometry/Source.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<math.h>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -18,7 +19,7 @@ class Shape
 public:
 	Shape(Color color) :color(color)
 	{
-		cout << "ShConstructor:\t" << this<<endl;
+		cout << "ShConstructor:\t" << this << endl;
 	}
 	virtual~Shape()
 	{
@@ -73,12 +74,123 @@ public:
 	}
 };
 
+class Rectangle :public Shape
+{
+	double side1;
+	double side2;
+public:
+	double get_side1()const
+	{
+		return side1;
+	}
+	double get_side2()const
+	{
+		return side2;
+	}
+
+	void set_side1(double side1)
+	{
+		if (side1 <= 0) side1 = 1;
+		this->side1 = side1;
+	}
+	void set_side2(double side2)
+	{
+		if (side2 <= 0) side2 = 1;
+		this->side2 = side2;
+	}
+	Rectangle(double side1, double side2, Color color) :Shape(color)
+	{
+		set_side1(side1);
+		set_side2(side2);
+	}
+	~Rectangle()
+	{
+
+	}
+	double get_area()const
+	{
+		return side1 * side2;
+	}
+	double get_perimeter()const
+	{
+		return (side1 + side2) * 2;
+	}
+	void draw()const
+	{
+		for (int i = 0; i < side1; i++)
+		{
+			for (int j = 0; j < side2; j++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+};
+
+class Triangle :public Shape
+{
+	double side;
+public:
+	double get_side()const
+	{
+		return side;
+	}
+
+	void set_side(double side)
+	{
+		if (side <= 0) side = 1;
+		this->side = side;
+	}
+	Triangle(double side, Color color) :Shape(color)
+	{
+		set_side(side);
+	}
+	~Triangle()
+	{
+
+	}
+	double get_area()const
+	{
+		return pow(side, 2) / 2;
+	}
+	double get_perimeter()const
+	{
+		return side * 3;
+	}
+	void draw()const
+	{
+		for (int i = 0; i < side; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+
+	}
+};
+
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 	Square square(5, Color::console_red);
 	cout << "Площадь квадрата: " << square.get_area() << endl;
-	cout<< "Периметр квадрата: " << square.get_perimeter() << endl;
+	cout << "Периметр квадрата: " << square.get_perimeter() << endl;
 	square.draw();
-	
+
+	Rectangle rectangle(5, 10, Color::console_red);
+	cout << "Площадь прямоугольника: " << rectangle.get_area() << endl;
+	cout << "Периметр прямоугольника: " << rectangle.get_perimeter() << endl;
+	rectangle.draw();
+
+	Triangle triangle(5, Color::console_red);
+	cout << "Площадь треугольника: " << triangle.get_area() << endl;
+	cout << "Периметр треугольника: " << triangle.get_perimeter() << endl;
+	triangle.draw();
+
+
+
 }
